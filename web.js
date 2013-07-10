8c79ae5fb2509a4ecef5087fb0dfb5fce7695eb4
David Hirsch <dehirsch@yahoo.com>
2013-07-10 06:56:00 +0000

    Fix syntax issues

--- web.js
+++ web.js
@@ -6,6 +6,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
